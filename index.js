1b74658fd14ed05f832f252c5dfe783334345f7a
tejashwini37 <tejashwini.rayudu37@gmail.com>
2023-06-30 06:29:46 +0000

    superleague

---
+++ index.js
@@ -0,0 +1,12 @@
+const element = (
+  //   Write your code here.
+  <div className="super-over-container">
+    <h1 className="super-over-heading">Super Over League</h1>
+    <div>
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
